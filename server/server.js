a09b00b37af37931970282763d04b50fea8b58ff
Raheim Bailey <raheimbailey@Raheims-MacBook-Pro.local>
2021-04-20 08:18:10 -0400

    delete useless variabled

--- server/server.js
+++ server/server.js
@@ -1,22 +1,13 @@
 
 const express = require('express');
-const path = require("path");
 const app = express();
 const matchedProjectsRoute = require('./routes/matchedProjects');
 const ongoingProjectsRoute = require('./routes/ongoingProjects');
 const completedProjectsRoute = require('./routes/completedProjects');
-// const cors = require('cors');
 
-require('dotenv').config();
+// require('dotenv').config();
 
-const port = process.env.PORT 
-
-
-// app.use(cors());
-
-app.use(express.json());
-
-app.use(express.static(path.join(__dirname, 'client')));
+const port = process.env.PORT || 3000
 
 app.get('/api', (req, res) => {
     res.send("Hello World")
